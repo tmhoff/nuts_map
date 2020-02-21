e1d4c71ab1644b3c1b848975236cc67c839874d8
Thomas <mail@tmhoff.net>
2020-02-21 15:58:05 +0100

    add tags and info text

--- ui.R
+++ ui.R
@@ -3,14 +3,21 @@ example_data <- readChar("data/example.txt", file.info("data/example.txt")$size)
 
 
 fluidPage(
+  tags$head(
+    tags$meta(charset="UTF-8"),
+    tags$meta(name="description", content="Online map generator to generate statistics maps using NUTS classification."),
+    tags$meta(name="keywords", content="generator, map, NUTS, NUTS 1, NUTS 2, NUTS 3, svg, map, europe, countries, statistics"),
+  ), 
   titlePanel("Nuts map generator"),
   hr(),
   fluidRow(
     column(1),
     column(4,
+           h5("Map generator for data using NUTS classification"),
+           hr(),
            textAreaInput(inputId = "data_raw",
                          label = "Copy raw data here:",
-                         value = example_data, width = '100%', height = '400px')),
+                         value = example_data, width = '100%', height = '350px')),
     column(7,
            plotOutput(outputId = "map"),
     )
